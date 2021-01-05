ca9a8b690cd386c0ba727c8b724e597334465539
ejone3 <76170430+ejone3@users.noreply.github.com>
2021-01-05 13:10:17 -0500

    Make large changes to the person.js file
    The "Person" was given a functionalists that allows it to determine if it has collided with any of the obstacles. It also now has a score and can keep track of it.

--- person.js
+++ person.js
@@ -7,8 +7,9 @@
 
 function Person() {
   this.pos = createVector(50, height-10);
-  this.vel = createVector(2, 0);
+  this.vel = createVector(0.25, 0);
   this.acc = createVector(0, 0);
+  this.score = 0;
 
   this.applyForce = function(force) {
     var f = force.copy();
@@ -26,8 +27,21 @@ function Person() {
     fill(255);
     stroke(255);
     rect(this.pos.x, this.pos.y, 10, 10);
+    
+    stroke(20, 20, 20);
+    textSize(100);
+    text(this.score, this.pos.x, 100);
   }
-
+  
+  this.hits = function(obs) {
+     if((obs.pos.x >= this.pos.x && obs.pos.x <= (this.pos.x + 10)) && (obs.pos.y >= this.pos.y && obs.pos.y <= (this.pos.y + 10))) {
+      this.score++;
+      obs.pos.y = -400;
+    }
+    //collideRectRect(this.pos.x, this.pos.y, 10, 10, obs.pos.x, obs.pos.y, 10, 10);
+  }
+  
+  // I changed some of these numbers so that my person is displayed on the ground properly
   this.edges = function() {
     if (this.pos.y > height - 11) {
       this.vel.y *= 0;
