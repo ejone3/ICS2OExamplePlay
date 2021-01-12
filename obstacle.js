dd95989f62a12a283cd960bce62e3cb5543ef0a9
ejone3 <76170430+ejone3@users.noreply.github.com>
2021-01-12 13:25:47 -0500

    Add a method
    I added a "hits" method to check if any of the "Obstacle" instances (or whatever you entered for "obs") were touching and then moving them back if they were.

--- obstacle.js
+++ obstacle.js
@@ -1,15 +1,15 @@
 class Obstacle {
-  constructor(v){
+  constructor(v) {
     this.pos = createVector(random(man.pos.x + 40, man.pos.x + 1200), random(height - 11));
     this.vel = createVector(-0.25, 0);
     this.acc = createVector(0, 0);
-    this.value = v||1;
+    this.value = v || 1;
     this.colour = color(random(255), random(255), random(255));
   }
-  
-  show(){
+
+  show() {
     noStroke();
-    if (this.colour === color("yellow")){
+    if (this.colour === color("yellow")) {
       fill(109, 189, 149);
       console.log("yes");
     } else {
@@ -18,11 +18,19 @@ class Obstacle {
     // changing these numbers makes the collision not work, why?
     rect(this.pos.x, this.pos.y, 10, 10) // try changing the numbers in person collision too
   }
-  
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
+  }
+
+  hits(obs) {
+    for (let i = 0; i > obs.length; i++) {
+      if ((obs[i].pos.x >= this.pos.x && obs[i].pos.x <= (this.pos.x + 10)) && (obs[i].pos.y >= this.pos.y && obs[i].pos.y <= (this.pos.y + 10))) {
+        obs.pos.x += 12;
+      }
+    }
   }
 }
 
@@ -31,7 +39,7 @@ class Special extends Obstacle {
     let v = 5;
     super(v);
   }
-  
+
   show() {
     noStroke();
     fill(255, 255, 0);
